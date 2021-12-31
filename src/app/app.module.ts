ea374367fe1387dbd1e778d12332caf0dc05651b
Olu Marshal <olmarsh.192@gmail.com>
2021-12-31 18:05:24 +0100

    2nd commit, added more interactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    UserItemComponent,
-    UserListComponent
+    ArticleComponent
   ],
   imports: [
     BrowserModule
